e0b6cdc22ae6960c0faef22402ea7c5ae072b706
Vladimir Kudinov <frux@yandex-team.ru>
2017-01-27 12:09:12 +0500

    add SVG loaders

--- webpack/client.config.js
+++ webpack/client.config.js
@@ -81,6 +81,22 @@ const config = {
 			{
 				test: /\.css$/,
 				loader: stylesLoader
+			},
+			{
+				test: /\.svg/,
+				use: [
+					'svg-url-loader',
+					{
+						loader: 'svgo-loader',
+						options: {
+							plugins: [
+								{removeTitle: true},
+								{convertColors: {shorthex: false}},
+								{convertPathData: false}
+							]
+						}
+					}
+				]
 			}
 		]
 	},
